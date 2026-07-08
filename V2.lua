f35bd408c014d53f39093761ec816cef80247048
Wardz <edchen114@gmail.com>
2026-07-08 16:25:20 +0700

    Update V2.lua

--- V2.lua
+++ V2.lua
@@ -4,5 +4,5 @@ local PlaceId = game.PlaceId
 if PlaceId == 129954712878723 then -- Market
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Punpunzero02/hydrahub/refs/heads/main/Market.lua"))()
 elseif PlaceId == 126884695634066 then -- ไอดีเกม Grow A Garden
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/wardz25/sphinxhub/refs/heads/main/main.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/wardz25/sphinxhub/refs/heads/main/mainv2.lua"))()
 end
