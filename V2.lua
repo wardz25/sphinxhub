960e779b653ee3e4d98983b3ec7986f23017496d
Wardz <edchen114@gmail.com>
2026-07-08 16:24:16 +0700

    Add files via upload

---
+++ V2.lua
@@ -0,0 +1,8 @@
+local game = game :: any
+local PlaceId = game.PlaceId
+
+if PlaceId == 129954712878723 then -- Market
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Punpunzero02/hydrahub/refs/heads/main/Market.lua"))()
+elseif PlaceId == 126884695634066 then -- ไอดีเกม Grow A Garden
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/wardz25/sphinxhub/refs/heads/main/main.lua"))()
+end
